374840cb3fcc2c5656605f3105dfb83fe382b9c2
Gavin <gavinfielder@gmail.com>
2018-03-28 10:57:07 -0700

    Observer pattern
    Implemented observer pattern for observable orders

--- keypadController.cpp
+++ keypadController.cpp
@@ -4,4 +4,10 @@ keypadController::keypadController(order * items)
 {
   cart = items;
   display.refresh(*cart);
+  cart->addObserver(this);
+}
+
+//is called when observed order changes
+void keypadController::update() {
+  display.refresh(*cart);
 }
